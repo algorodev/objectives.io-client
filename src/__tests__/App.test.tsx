1b9101fd3d07582786ae7917402ca43d0bfe885e
Alex González <me@algorodev.com>
2023-06-25 13:26:55 +0200

    [TEST] App component test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,25 +1,33 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import App from "../App";
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from '../App'
 
-describe("App Component", () => {
-  it("should mount App component as expected", () => {
-    render(<App />);
-
-    expect(screen.getByText("Vite + React")).toBeInTheDocument();
-    expect(
-      screen.getByText(/Click on the Vite and React logos to learn more/i)
-    ).toBeInTheDocument();
-  });
-
-  it("should sum 1 on button click", async () => {
-    const user = userEvent.setup();
-
-    render(<App />);
-
-    await user.click(screen.getByRole("button"));
-
-    expect(screen.getByRole("button").textContent).toBe("count is 1");
-  });
-});
+describe('App Component', () => {
+	it('should mount App component as expected', async () => {
+		render(<App />)
+		const h1Element = screen.getByText('Welcome to Objectives.io', {
+			selector: 'h1',
+		})
+		const h2Element = screen.getByText('Welcome to Objectives.io', {
+			selector: 'h2',
+		})
+		const h3Element = screen.getByText('Welcome to Objectives.io', {
+			selector: 'h3',
+		})
+		const h4Element = screen.getByText('Welcome to Objectives.io', {
+			selector: 'h4',
+		})
+		const pElement = screen.getByText('Welcome to Objectives.io', {
+			selector: 'p',
+		})
+		const smallElement = screen.getByText('Welcome to Objectives.io', {
+			selector: 'small',
+		})
+		expect(h1Element).toBeInTheDocument()
+		expect(h2Element).toBeInTheDocument()
+		expect(h3Element).toBeInTheDocument()
+		expect(h4Element).toBeInTheDocument()
+		expect(pElement).toBeInTheDocument()
+		expect(smallElement).toBeInTheDocument()
+	})
+})
