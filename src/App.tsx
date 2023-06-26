c6108218d53eb6f28a205f44dd4a4a3509bba711
Alex González <me@algorodev.com>
2023-06-26 17:58:20 +0200

    [FEATURE] Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
+import Home from './pages/home/Home'
 import './App.css'
 
 const App = () => (
-	<div>
-		<h1>Welcome to Objectives.io</h1>
-		<h2>Welcome to Objectives.io</h2>
-		<h3>Welcome to Objectives.io</h3>
-		<h4>Welcome to Objectives.io</h4>
-		<p>Welcome to Objectives.io</p>
-		<small>Welcome to Objectives.io</small>
-	</div>
+	<main className='app'>
+		<Home />
+	</main>
 )
 
 export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import Container from '../../components/container/Container'
+
+const Home = () => {
+	return (
+		<Container>
+			<h4>Container works!</h4>
+		</Container>
+	)
+}
+
+export default Home
